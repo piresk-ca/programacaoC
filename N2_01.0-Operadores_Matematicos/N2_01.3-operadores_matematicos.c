1f1cb52a760a8801374a81270b666e368d90a4a8
piresk-ads <carol.pires.ads@gmail.com>
2026-05-31 15:24:44 +0000

    atualização dos operadores

--- N2_01.0-Operadores_Matematicos/N2_01.3-operadores_matematicos.c
+++ N2_01.0-Operadores_Matematicos/N2_01.3-operadores_matematicos.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
+
 int main() {
-/*
-Incremento (++)
-Pré-incremento ++a
-Pós-incremento a++
-Decremento (--)
-Pré-decremento --a
-Pós-decremento a--
-*/
+    /*
+    Incremento (++)
+    Pré-incremento ++a
+    Pós-incremento a++
+    Decremento (--)
+    Pré-decremento --a
+    Pós-decremento a--
+    */
 
-int numero1 = 1, resultado;
+    int numero1 = 1, resultado;
 
-printf("Antes incremento: %d\n", numero1);
-//numero1 = numero1 + 1; 
-// ou numero1 += 1;
-//Pos incremento;
-//resultado = numero1 + 1;
-//numero++;
-resultado = ++numero1;
-//printf("Apos incremento: %d\n", numero1);
-printf("Após pré-incremento - Número 1: %d\n Resultado: %d\n", numero1, resultado);
+    printf("Antes incremento: %d\n", numero1);
+    //numero1 = numero1 + 1; 
+    //numero1 += 1;
+    //Pos incremento;
+    //resultado = numero1 + 1;
+    //numero++;
+    resultado = numero1++;
+    //printf("Apos incremento: %d\n", numero1);
+    printf("Após pós-incremento - Número 1: %d - Resultado: %d\n", numero1, resultado);
 
+    resultado = ++numero1;
+    printf("Após pré-incremento - Número 1: %d - Resultado: %d\n", numero1, resultado);
 
-//numero1 = numero1 - 1;
-// ou numero1 -= 1;
-numero1--;
-printf("Após decremento: %d\n", numero1);
+    //numero1 = numero1 - 1;
+    //numero1 -= 1;
+    //numero1--;
+    //printf("Após decremento: %d\n", numero1);
+    resultado = numero1--;
+    printf("Após pós-decremento - Número 1: %d - Resultado: %d\n", numero1, resultado);
+
+    resultado = --numero1;
+    printf("Após pré-decremento - Número 1: %d - Resultado: %d\n", numero1, resultado);
+    
 
 
 }
